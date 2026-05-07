64682c7460321d578691e05dbe708612d4bf3719
drmathias <adamshirt@outlook.com>
2026-05-07 20:51:51 +0100

    Setup action to deploy to GitHub pages

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const SITE = {
-  website: "https://www.latentbytes.net/", // replace this with your deployed domain
+  website: "https://www.latentbytes.net/",
   author: "Adam Shirt",
   profile: "https://cv.64815139.xyz",
   desc: "A personal blog about computing, software, and the occasional economics tangent.",
@@ -9,7 +9,7 @@ export const SITE = {
   postPerPage: 4,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: false,
-  showBackButton: true, // show back button in post detail
+  showBackButton: true,
   editPost: {
     enabled: true,
     text: "Edit page",
@@ -17,6 +17,6 @@ export const SITE = {
   },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
-  lang: "en", // html lang code. Set this empty and default will be "en"
+  lang: "en",
   timezone: "Europe/London", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
